4993d4c600c7a5f79236f13eb34e2b01162a1840
S1ngle777 <nniebogha@gmail.com>
2025-06-15 13:36:43 +0300

    Remove dashboard route from web.php

--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 // Маршруты для аутентифицированных пользователей
 Route::middleware(['auth'])->group(function () {
     Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
